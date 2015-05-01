63d7ad22c6db1f1b32aee6c9b8508149c5c8ea8a
unknown <wattersonglendon@yahoo.co.uk>
2015-05-01 21:25:41 +0100

    added some controllers and images

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('cavamenu', ['ionic', 'cavamenu.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -20,5 +20,29 @@ angular.module('cavamenu', ['ionic', 'cavamenu.controllers'])
     }
   });
 
-  $urlRouterProvider.otherwise('/app/dishes/1');
+   $urlRouterProvider.otherwise('/app/dishes/1');
 });
+
+function incDec(plusMinus) {
+      var plusMinus = parseInt(plusMinus);
+      var calorieContent = 
+parseInt(document.getElementById("dailyTotal").innerHTML) + plusMinus;
+      //alert(calorieContent);
+    
+     if(calorieContent < 0)
+     {
+       calorieContent = 0;
+     }  
+     else if(calorieContent > 3000)
+     {
+       calorieContent = 3000;
+     }
+     else
+     {
+       document.getElementById("dailyTotal").innerHTML = calorieContent;
+     }
+   }
+    
+    function reset() {
+      document.getElementById("dailyTotal").innerHTML = 00;
+    } 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,66 +1,9 @@
+angular.module('starter.controllers', [])
 
-angular.module('cavamenu.controllers', [])
-
-.controller('AppCtrl', function($scope) {
-  $scope.categories = ['Snacks', 'Fruit', 'Vegetables', 'Meats', 'Breads','Drinks']
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 })
 
 .controller('DishesCtrl', function($scope, $stateParams) {
-  categories = ['Snacks', 'Fruit', 'Vegetables', 'Meats', 'Breads','Drinks'];
-
-  dishes = [
-    [
-	
-    "icecream",
-      "crisps",
-      "chocolate", 
-	  "toffee",
-      "cake",
-    ],
-    [
-      "Apples",
-      "Oranges",
-      "Banana",
-      "Grapes",
-      
-   ],
-   [
-      "Potatoes",
-      "Mushrooms",
-      "Peppers",
-      "Parsnips",
-      "Onions",
-      
-      
-    ],
-    [
-      "Mince meat",
-      "Beef burgers",
-      "Chicken Breast",
-      
-      "Sausages",
-      ],
-	  [
-	  "Brown Bread",
-	  "Wholemeal Bread",
-	  "Ciabatta",
-	  "White Slice Bread",
-	  "Tortilla Wraps",
-	  "Pitta Pockets",
-	  ],
-	  [
-	  "Bottled Beer",
-	  "Glass of Milk(full fat)",
-	  "Orange Juice",
-	  "Coca Cola",
-	  "Coffee",
-	  "Tea",
-	  "Small Glass of Wine",
-   ]
-  ];
-  
- $scope.category = categories[$stateParams.dishesId];
-  $scope.dishes = dishes[$stateParams.dishesId];
-
+  $scope.dishesId = $stateParams.dishesId; 
 });
 
